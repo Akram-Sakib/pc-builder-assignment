771e6582521ac5423df2b05f01a4dffc235c9584
akram-sakib <sayedakramsakib@gmail.com>
2023-07-30 12:08:01 +0600

    :tada: initial commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
+import Layout from "@/components/Layout/Layout";
+import { wrapper } from "@/redux/app/store";
+import { SessionProvider } from "next-auth/react";
+import { Provider } from "react-redux";
+import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  const { store } = wrapper.useWrappedStore(pageProps);
+
+  return (
+    <SessionProvider session={session}>
+      <Provider store={store}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
+    </SessionProvider>
+  );
 }
